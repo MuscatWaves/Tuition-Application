e34bedd4d3dbb445004e84f076aa2d801c217902
Recruitment <prabinkumar1999@outlook.com>
2022-07-07 10:03:00 +0400

    Main branch updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./app.css";
+import "./App.css";
 import Routing from "./routes";
 import { HelmetProvider } from "react-helmet-async";
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,15 +2,7 @@ import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loader from "../components/Loader";
 
-const Login = lazy(() => import("../pages/Login"));
-const DashBoard = lazy(() => import("../pages/DashBoard"));
-const BuildCV = lazy(() => import("../pages/BuildCV"));
-const SearchCV = lazy(() => import("../pages/SearchCV"));
-const CVprofile = lazy(() => import("../pages/CVProfile"));
-const UserManage = lazy(() => import("../pages/UserManage"));
-const UserReport = lazy(() => import("../pages/UserReport"));
-const RejectedCV = lazy(() => import("../pages/RejectedCV"));
-const UploadCV = lazy(() => import("../pages/UploadCV"));
+const LandingPage = lazy(() => import("../pages/LandingPage"));
 const PageNotFound = lazy(() => import("../components/NoPageFound"));
 
 const Routing = () => {
@@ -19,20 +11,7 @@ const Routing = () => {
       <Suspense fallback={<Loader minHeight={"90vh"} />}>
         <Router>
           <Routes>
-            <Route path="/" element={<Login />}></Route>
-            <Route path="/dashboard" element={<DashBoard />}></Route>
-            <Route path="/cv/create" element={<BuildCV />}></Route>
-            <Route path="/cv/update/:id" element={<BuildCV />}></Route>
-            <Route path="/searchcv" element={<SearchCV />}></Route>
-            <Route
-              path="/searchcv/profile/:type/:id"
-              element={<CVprofile />}
-            ></Route>
-            <Route path="/cv/:id/:name" element={<CVprofile />}></Route>
-            <Route path="/userManage" element={<UserManage />}></Route>
-            <Route path="/userReport" element={<UserReport />}></Route>
-            <Route path="/rejectedcv" element={<RejectedCV />}></Route>
-            <Route path="/uploadcv" element={<UploadCV />}></Route>
+            <Route path="/" element={<LandingPage />}></Route>
             <Route path="*" element={<PageNotFound />}></Route>
           </Routes>
         </Router>
