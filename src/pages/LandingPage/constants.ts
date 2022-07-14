1a9006e6f0f65a564c15fef29213b54d705b0630
Recruitment <prabinkumar1999@outlook.com>
2022-07-14 11:33:45 +0400

    Landing page updated

---
+++ src/pages/LandingPage/constants.ts
@@ -0,0 +1,25 @@
+export const cardList = [{
+    id: 1,
+    name: "Tution Classes",
+    description: "Guess it's something related to Tution Classes"
+},{
+    id: 2,
+    name: "Tution Classes",
+    description: "Guess it's something related to Tution Classes"
+},{
+    id: 3,
+    name: "Tution Classes",
+    description: "Guess it's something related to Tution Classes"
+},{
+    id: 4,
+    name: "Tution Classes",
+    description: "Guess it's something related to Tution Classes"
+},{
+    id: 5,
+    name: "Tution Classes",
+    description: "Guess it's something related to Tution Classes"
+},{
+    id: 6,
+    name: "Tution Classes",
+    description: "Guess it's something related to Tution Classes"
+},]
--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TopNavigation from "../../components/TopNavigation";
 import { Button } from "primereact/button";
+import { cardList } from "./constants.ts";
 import "./landingpage.css";
 
 const LandingPage = () => {
@@ -11,9 +12,9 @@ const LandingPage = () => {
         <div className="first-section">
           <div className="flex-gap-column-2">
             <div>
-              <h1 className="text primary-text-colour boldest big-text">
+              <div className="text primary-font primary-text-colour big-text">
                 Ready to learn
-              </h1>
+              </div>
             </div>
             <div className="primary-text-colour middile-text">
               You've got the will. We've got the way. We will help you towards
@@ -36,7 +37,27 @@ const LandingPage = () => {
             ></iframe>
           </div>
         </div>
-        <div className="second-section"></div>
+        <div className="second-section">
+          <div className="second-section-header">
+            <div className="secondary-text-colour bolder">
+              Sharing the light
+            </div>
+            <div className="larger-text bolder primary-text-colour">
+              Services we provide
+            </div>
+          </div>
+          <div className="card-pack">
+            {cardList.map((card) => (
+              <div className="each-card" key={card.key}>
+                <div></div>
+                <div>
+                  <div>{card.name}</div>
+                  <div>{card.description}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
