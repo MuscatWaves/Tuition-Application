f73d816acf1ce2c7aeb11c58ad64512ac3ed6467
Recruitment <prabinkumar1999@outlook.com>
2022-07-25 14:55:17 +0400

    Landing page updated

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import ojimage from "../../images/brush.webp";
 import "./loader.css";
 import { ProgressSpinner } from "primereact/progressspinner";
-import { motion } from "framer-motion";
+import { m, LazyMotion, domAnimation } from "framer-motion";
 
 const Loader = ({ minHeight }) => {
   useEffect(() => {
@@ -10,19 +10,21 @@ const Loader = ({ minHeight }) => {
   }, []);
 
   return (
-    <motion.div
-      className="loading-data"
-      style={{ minHeight: minHeight }}
-      animate={{ opacity: 1 }}
-      initial={{ opacity: 0 }}
-      transition={{ duration: 0.2 }}
-      exit={{ opacity: 0 }}
-    >
-      <div className="inner-loading-data">
-        <img src={ojimage} className="loader-image" alt="Oman jobs" />
-        <ProgressSpinner />
-      </div>
-    </motion.div>
+    <LazyMotion features={domAnimation}>
+      <m.div
+        className="loading-data"
+        style={{ minHeight: minHeight }}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        exit={{ opacity: 0 }}
+      >
+        <div className="inner-loading-data">
+          <img src={ojimage} className="loader-image" alt="Oman jobs" />
+          <ProgressSpinner />
+        </div>
+      </m.div>
+    </LazyMotion>
   );
 };
 
