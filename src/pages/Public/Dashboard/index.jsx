dc429f55ecf9553e9527816ad682800a1089dbc1
PrabinKumar19 <prabinkumar1999@outlook.com>
2022-11-12 17:44:35 +0400

    page updated

--- src/pages/Public/Dashboard/index.jsx
+++ src/pages/Public/Dashboard/index.jsx
@@ -6,11 +6,18 @@ import { cards, container, item } from "./constants";
 import { FaBook, FaUserCircle } from "react-icons/fa";
 import { MdOutlineGrade } from "react-icons/md";
 import { FiMail } from "react-icons/fi";
+import { removeUnderScore } from "../../../utilities";
+import { useQuery } from "react-query";
+import Cookies from "universal-cookie";
+import axios from "axios";
+import moment from "moment";
 import "./dashboard.css";
 
 const Dashboard = () => {
   // const [isLoggedIn, setLoggedIn] = useState({});
   const isLoggedIn = {};
+  const cookies = new Cookies();
+  const token = cookies.get("token");
   const navigate = useNavigate();
   const navigateTo = (path) => {
     navigate(path);
@@ -18,7 +25,54 @@ const Dashboard = () => {
   const checkNumberOfCards = () =>
     cards(isLoggedIn).filter((card) => card?.permission).length;
 
+  const {
+    data: basicDetApi = {},
+    // isFetching: basicDetFetching,
+    // refetch: basicDetRefetch,
+  } = useQuery(
+    ["basicDetailsFetch"],
+    () =>
+      axios.get(`/api/studentprofile/basic`, {
+        headers: {
+          Authorization: token,
+        },
+      }),
+    { refetchOnWindowFocus: false, select: (data) => data.data.data[0] }
+  );
+
+  const {
+    data: eduDetApi = {},
+    // isFetching,
+    // refetch,
+  } = useQuery(
+    ["educationDetailsFetch"],
+    () =>
+      axios.get(`/api/studentprofile/education`, {
+        headers: {
+          Authorization: token,
+        },
+      }),
+    { refetchOnWindowFocus: false, select: (data) => data.data.data[0] }
+  );
+
+  console.log(eduDetApi);
+
   // https://img.youtube.com/vi/sUwD3GRPJos/maxresdefault.jpg
+
+  const basicDetails = {
+    gender: basicDetApi?.Gender,
+    createdAt: moment(basicDetApi?.createdAt).format("DD MMM, YYYY"),
+    date_of_birth: moment(basicDetApi?.dob).format("DD MMM, YYYY"),
+    location: basicDetApi?.country,
+  };
+
+  const educationDetail = {
+    course: eduDetApi?.course,
+    specialization: eduDetApi?.specialization,
+    university: eduDetApi?.university,
+    location: eduDetApi?.location,
+    passingYear: eduDetApi?.passingYear,
+  };
 
   return (
     <m.div
@@ -46,27 +100,31 @@ const Dashboard = () => {
               Prabin Kumar Pradeep
             </div>
             <div className="tuition-wt-inner">
-              <div className="flex-small-gap">
-                <FiMail
-                  style={{
-                    width: "20px",
-                    height: "20px",
-                    color: "var(--red-shade-color)",
-                  }}
-                />
-                <div className="small-text bold">
-                  prabinkumar1999@outlook.com
+              <div>
+                <div className="flex-small-gap">
+                  <FiMail
+                    style={{
+                      width: "20px",
+                      height: "20px",
+                      color: "var(--red-shade-color)",
+                    }}
+                  />
+                  <div className="small-text bold red-shade-colour">Email</div>
                 </div>
+                <div className="bold">prabinkumar1999@outlook.com</div>
               </div>
-              <div className="flex-small-gap">
-                <MdOutlineGrade
-                  style={{
-                    width: "20px",
-                    height: "20px",
-                    color: "var(--red-shade-color)",
-                  }}
-                />
-                <div className="small-text bold">Grade 2</div>
+              <div>
+                <div className="flex-small-gap">
+                  <MdOutlineGrade
+                    style={{
+                      width: "20px",
+                      height: "20px",
+                      color: "var(--red-shade-color)",
+                    }}
+                  />
+                  <div className="small-text bold red-shade-colour">Grade</div>
+                </div>
+                <div className="bold">Grade 2</div>
               </div>
             </div>
           </div>
@@ -76,8 +134,28 @@ const Dashboard = () => {
         </div>
         <div className="mtd-main-box">
           <div className="mtd-main-box__inner">
-            <div className="mtd-main-box__b-1">Personal Details</div>
-            <div className="mtd-main-box__b-2">Recent Purchases</div>
+            <div className="mtd-main-box__b-1">
+              <div>Basic Details</div>
+              <div className="mtd-main-box__b-1__grid-2">
+                {Object.keys(basicDetails).map((detail) => (
+                  <div key={detail}>
+                    <div>{removeUnderScore(detail)}</div>
+                    <div>{basicDetails[detail]}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="mtd-main-box__b-2">
+              <div>Education Details</div>
+              <div className="mtd-main-box__b-1__grid-2">
+                {Object.keys(educationDetail).map((detail) => (
+                  <div key={detail}>
+                    <div>{removeUnderScore(detail)}</div>
+                    <div>{educationDetail[detail]}</div>
+                  </div>
+                ))}
+              </div>
+            </div>
             <div className="mtd-main-box__b-3">A buy icon</div>
           </div>
           <div>My Subjects</div>
@@ -89,8 +167,7 @@ const Dashboard = () => {
 
 export default Dashboard;
 
-{
-  /* <AnimatePresence>
+/* <AnimatePresence>
             <m.div
               className={
                 checkNumberOfCards() > 4 ? "main-card grid-3" : "main-card"
@@ -123,4 +200,3 @@ export default Dashboard;
               )}
             </m.div>
           </AnimatePresence> */
-}
