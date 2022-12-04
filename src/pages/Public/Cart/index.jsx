7e74365b404296e7a9a49afc59da2e3fb30c116b
MW-IT <prabinkumar1999@outlook.com>
2022-12-04 13:17:42 +0400

    page updated

--- src/pages/Public/Cart/index.jsx
+++ src/pages/Public/Cart/index.jsx
@@ -2,25 +2,43 @@ import React, { useState, useEffect } from "react";
 import { m } from "framer-motion";
 import BreadCrumb from "../../../components/BreadCrumb";
 import Header from "../../../components/Header";
-import { Modal } from "@mantine/core";
+import { Divider, Modal } from "@mantine/core";
 import Cookies from "universal-cookie";
 import CustomButton from "../../../components/Buttons";
 import { greenNotify, redNotify } from "../../../notification";
 import { showNotification } from "@mantine/notifications";
-import { FaBook, FaOpencart } from "react-icons/fa";
+import { FaBook, FaOpencart, FaCheck } from "react-icons/fa";
 import { container, item } from "../Dashboard/constants";
 import { CgRemove } from "react-icons/cg";
 import Spinner from "../../../components/Spinner";
+import { BiErrorCircle } from "react-icons/bi";
 import { useQuery } from "react-query";
 import axios from "axios";
 import "../Shop/shop.css";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { useNavigate } from "react-router";
 
 function Cart() {
+  const navigateTo = useNavigate();
   const cookies = new Cookies();
   const token = cookies.get("token");
   const [isDeleteData, setDeleteData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [deleteModal, setDeleteModal] = useState(false);
+  const [amount, setAmount] = useState(0);
+  const [success, setSuccess] = useState(false);
+  const [statusError, setStatusError] = useState(false);
+  const [ErrorMessage, setErrorMessage] = useState("");
+  const [payer, setPayer] = useState({});
+  const [orderID, setOrderID] = useState(false);
+  const [checkoutProceed, setCheckoutProceed] = useState(false);
+  const [list, setList] = useState([]);
+  const PayPalOptions = {
+    "client-id":
+      "AaK0yvg1GLTpbKiX6sBGzwXEgvKVEUfWFqVvaspDAXRj6vUqV80IIuU5PfP1uXxLqt15YlfohrJ9aVzL",
+    currency: "USD",
+    intent: "capture",
+  };
   const navigation = [
     { id: 0, name: "Dashboard", url: "/dashboard" },
     {
@@ -41,7 +59,7 @@ function Cart() {
   }, []);
 
   const {
-    data = [],
+    data: cartData = [],
     isFetching,
     refetch,
   } = useQuery(
@@ -96,6 +114,59 @@ function Cart() {
     setDeleteModal(false);
   };
 
+  // creates a paypal order
+  const createOrder = (data, actions) => {
+    setList(cartData.data);
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            description: "Sunflower",
+            amount: {
+              currency_code: "USD",
+              value: amount,
+            },
+          },
+        ],
+        // not needed if a shipping address is actually needed
+        application_context: {
+          shipping_preference: "NO_SHIPPING",
+        },
+      })
+      .then((orderID) => {
+        setOrderID(orderID);
+        return orderID;
+      });
+  };
+
+  // check Approval
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then(function (details) {
+      const { payer } = details;
+      setPayer(payer);
+      setSuccess(true);
+    });
+  };
+
+  //capture likely error
+  const onError = (data, actions) => {
+    return actions.order.capture().then(function (details) {
+      const { payer } = details;
+      setPayer(payer);
+      setCheckoutProceed(false);
+      setStatusError(true);
+      setErrorMessage(
+        "An Error occured with your payment. Please try again after sometime!"
+      );
+    });
+  };
+
+  useEffect(() => {
+    if (cartData && cartData?.ToatalAmount > 0) {
+      setAmount(cartData?.ToatalAmount);
+    }
+  }, [cartData]);
+
   return (
     <m.div
       className="shp"
@@ -123,6 +194,133 @@ function Cart() {
           />
         </div>
       </Modal>
+      <Modal
+        title={<div className="bolder large-text">Payment Error</div>}
+        opened={statusError}
+        onClose={setStatusError}
+        centered
+        size={"auto"}
+      >
+        <Divider
+          my="lg"
+          label={
+            <div className="bold medium-text primary-colour">
+              Payment Details
+            </div>
+          }
+        />
+        <div className="order-unsucessful">
+          <BiErrorCircle className="unsuccess-icn" />
+          <div>
+            <div className="small-text primary-colour bold">Order Id</div>
+            <div className="bold">{orderID}</div>
+          </div>
+          <div>
+            <div className="small-text primary-colour bold">Name</div>
+            <div className="bold">
+              {payer?.name?.given_name + " " + payer?.name?.surname}
+            </div>
+          </div>
+          <div>
+            <div className="small-text primary-colour bold">Email</div>
+            <div className="bold">{payer?.email_address}</div>
+          </div>
+        </div>
+        <div className="flex-center small-padding text-red bold">
+          {ErrorMessage}
+        </div>
+        <div style={{ padding: "1rem 0" }} className="flex-center">
+          <CustomButton
+            loading={loading}
+            label={"Go back to cart"}
+            action={() => setStatusError(false)}
+          />
+        </div>
+      </Modal>
+      <Modal
+        title={<div className="bolder large-text">Order Successful</div>}
+        opened={success}
+        onClose={() => navigateTo("/dashboard")}
+        centered
+        size={"auto"}
+      >
+        <Divider
+          my="lg"
+          label={
+            <div className="bold medium-text primary-colour">
+              Payment Details
+            </div>
+          }
+        />
+        <div className="order-sucessful">
+          <FaCheck className="success-icn" />
+          <div>
+            <div className="small-text primary-colour bold">Order Id</div>
+            <div className="bold">{orderID}</div>
+          </div>
+          <div>
+            <div className="small-text primary-colour bold">Name</div>
+            <div className="bold">
+              {payer?.name?.given_name + " " + payer?.name?.surname}
+            </div>
+          </div>
+          <div>
+            <div className="small-text primary-colour bold">Email</div>
+            <div className="bold">{payer?.email_address}</div>
+          </div>
+        </div>
+        <Divider
+          my="lg"
+          label={
+            <div className="bold medium-text primary-colour">
+              Ordered Topics
+            </div>
+          }
+        />
+        <m.div
+          className="shp-list-main"
+          variants={container}
+          initial="hidden"
+          animate="show"
+        >
+          {list?.map((subject) => (
+            <m.div
+              className="shp-list-main__each"
+              key={subject.subjectId}
+              variants={item}
+            >
+              <FaBook className="shp-list-icn" />
+              <div className="flex-small-gap-column">
+                <div className="large-text bold red-shade-colour">
+                  {subject.subjectTitle}
+                </div>
+                <div className="small-text bold primary-colour">
+                  {subject.subjectDescription}
+                </div>
+              </div>
+              <div className="flex-small-gap-column">
+                <div className="small-text primary-colour bold">Standard</div>
+                <div className="bolder">{subject.subjectStandard}</div>
+              </div>
+              <div className="flex-small-gap-column">
+                <div className="small-text primary-colour bold">Price</div>
+                <div className="bolder">{subject.subjectPrice}</div>
+              </div>
+            </m.div>
+          ))}
+        </m.div>
+        <div
+          style={{ padding: "1rem 0" }}
+          className="flex-small-gap flex-center"
+        >
+          {/* <CustomButton action={handleClose} label={"Cancel"} color={"gray"} /> */}
+          <CustomButton
+            loading={loading}
+            label={"Proceed to Dashboard"}
+            action={() => navigateTo("/dashboard")}
+          />
+        </div>
+      </Modal>
       <Header />
       <div className="shp-wrapper">
         <BreadCrumb items={navigation} />
@@ -146,7 +344,7 @@ function Cart() {
               initial="hidden"
               animate="show"
             >
-              {data.data.map((subject) => (
+              {cartData.data.map((subject) => (
                 <m.div
                   className="shp-list-main__each"
                   key={subject.subjectId}
@@ -183,30 +381,53 @@ function Cart() {
                 </m.div>
               ))}
             </m.div>
-            <m.div
-              className="shp-list-main__total-amount"
-              animate={{ opacity: 1, y: "0" }}
-              initial={{ opacity: 0, y: "20px" }}
-              transition={{ duration: "0.8" }}
-            >
-              <div className="primary-colour">
-                {data.data.length + ` item('s) present within cart`}
-              </div>
-              {data.ToatalAmount ? (
-                <div className="flex-gap bold shp-list-main__amount">
-                  <div className="large-text">
-                    Amount to be paid : <span>{data.ToatalAmount}</span> OMR
-                  </div>
-                  <CustomButton
-                    label={"Proceed to checkout"}
-                    color="teal"
-                    radius={"xl"}
-                  />
+            {checkoutProceed ? (
+              <m.div
+                className="shp-list-main__total-amount"
+                animate={{ opacity: 1, y: "0" }}
+                initial={{ opacity: 0, y: "20px" }}
+                transition={{ duration: "0.8" }}
+              >
+                <div className="mid-large-text bold">
+                  Choose your payment method
                 </div>
-              ) : (
-                <div className="large-text">No Subjects in Cart</div>
-              )}
-            </m.div>
+                <PayPalScriptProvider options={PayPalOptions}>
+                  <PayPalButtons
+                    style={{ layout: "vertical" }}
+                    createOrder={createOrder}
+                    onApprove={onApprove}
+                    onError={onError}
+                  />
+                </PayPalScriptProvider>
+              </m.div>
+            ) : (
+              <m.div
+                className="shp-list-main__total-amount"
+                animate={{ opacity: 1, y: "0" }}
+                initial={{ opacity: 0, y: "20px" }}
+                transition={{ duration: "0.8" }}
+              >
+                <div className="primary-colour">
+                  {cartData.data.length + ` item('s) present within cart`}
+                </div>
+                {cartData.ToatalAmount ? (
+                  <div className="flex-gap bold shp-list-main__amount">
+                    <div className="large-text">
+                      Amount to be paid : <span>{cartData.ToatalAmount}</span>{" "}
+                      OMR
+                    </div>
+                    <CustomButton
+                      label={"Proceed to checkout"}
+                      color="teal"
+                      radius={"xl"}
+                      action={() => setCheckoutProceed(true)}
+                    />
+                  </div>
+                ) : (
+                  <div className="large-text">No Subjects in Cart</div>
+                )}
+              </m.div>
+            )}
           </>
         )}
       </div>
